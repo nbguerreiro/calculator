8372be8fce2dde3fb3896bd356965467ad1275b3
nbguerreiro <nbguerreiro@sapo.pt>
2021-03-12 16:21:19 +0000

    varios

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+    return Number(a) - Number(b);
+}
+
+function multiply(a, b) {
+    return Number(a) * Number(b);
+}
+
+function divide(a, b) {
+    return Number(a) / Number(b);
+}
+
+
+function operate(op, num1, num2) {
+    return op(num1, num2);
+}
