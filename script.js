87954c5cf95e1d0026c0b7f0035b86c83352cc3b
nbguerreiro <nbguerreiro@sapo.pt>
2021-03-13 14:21:07 +0000

    2021-03-13_14:21

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ function divide(a, b) {
 function operate(op, num1, num2) {
     return op(num1, num2);
 }
+
+const monitor = document.querySelector('.monitor p');
+let monitorText = "";
+
+function monitorPaint(val) {
+    if (val === 'RESET') {
+        monitorText = "";
+    } else {
+        monitorText = monitorText + val;
+    }
+    monitor.textContent = monitorText;
+}
+
+const buttons = document.querySelectorAll('.container button');
+buttons.forEach(element => {
+    element.addEventListener('click', () => {
+        let val = element.value;
+        // console.log(val);
+        monitorPaint(val);
+    })
+});
